30d9c3eb7240c69ea25a19a36de766cf99a2bcaf
Samuel Utbult <samuel.utbult@hotmail.com>
2019-07-29 16:39:39 +0000

    Add MaxAngle constant

--- Assets/Cannon.cs
+++ Assets/Cannon.cs
@@ -6,6 +6,7 @@ public class Cannon : MonoBehaviour
 {
     private int RotationDirection { get; set; }
     private const float RotationSpeed = 100;
+    private const float MaxAngle = 45;
     
     // Start is called before the first frame update
     void Start()
@@ -18,14 +19,14 @@ public class Cannon : MonoBehaviour
     {
         var diff = RotationSpeed * RotationDirection * Time.deltaTime;
         SetCannonAngle(GetCannonAngle() + diff);
-        if (GetCannonAngle() < -45)
+        if (GetCannonAngle() < -MaxAngle)
         {
-            SetCannonAngle(-45);
+            SetCannonAngle(-MaxAngle);
             RotationDirection = 1;
         }
-        else if (GetCannonAngle() > 45)
+        else if (GetCannonAngle() > MaxAngle)
         {
-            SetCannonAngle(45);
+            SetCannonAngle(MaxAngle);
             RotationDirection = -1;
         }
     }
