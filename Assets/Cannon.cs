807dbd05d6a20c0c3d689511b28a300bcd433947
Samuel Utbult <samuel.utbult@hotmail.com>
2019-07-29 17:13:24 +0000

    Add working cannon

--- Assets/Cannon.cs
+++ Assets/Cannon.cs
@@ -4,35 +4,50 @@ using UnityEngine;
 
 public class Cannon : MonoBehaviour
 {
-    private bool Rotates { get; set; }
-    private int RotationDirection { get; set; }
-    private const float RotationSpeed = 100;
+    public GameObject cannonBall;
+    private bool _rotates;
+    private int _rotationDirection;
+    private const float RotationSpeed = 10;
     private const float MaxAngle = 45;
     
     // Start is called before the first frame update
     void Start()
     {
-        RotationDirection = 1;
-        Rotates = true;
+        _rotationDirection = 1;
+        _rotates = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Rotates)
+        if (_rotates)
         {
-            var diff = RotationSpeed * RotationDirection * Time.deltaTime;
+            var diff = RotationSpeed * _rotationDirection * Time.deltaTime;
             SetCannonAngle(GetCannonAngle() + diff);
             if (GetCannonAngle() < -MaxAngle)
             {
                 SetCannonAngle(-MaxAngle);
-                RotationDirection = 1;
+                _rotationDirection = 1;
             }
             else if (GetCannonAngle() > MaxAngle)
             {
                 SetCannonAngle(MaxAngle);
-                RotationDirection = -1;
+                _rotationDirection = -1;
             } 
+        }
+
+        if (Input.GetButtonDown("2_player_toggleTurning"))
+        {
+            _rotates = !_rotates;
+        }
+        
+        if (Input.GetButtonDown("2_player_fire"))
+        {
+            var ball = Instantiate(cannonBall);
+            var rigidBody = ball.GetComponent<Rigidbody>();
+            var muzzle = transform.Find("Muzzle");
+            ball.transform.position = muzzle.position;
+            rigidBody.AddForce(transform.forward * 10, ForceMode.Impulse);
         }
     }
 
